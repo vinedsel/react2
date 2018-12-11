c532714fbce9dc465549a3593005e3cb33e2ca80
Raymond Gregory <vinedsel@gmail.com>
2018-12-11 14:43:07 -0600

    Modded JS file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-alert("it's alive");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <ul>
+    <li>Apples</li>
+    <li>Oranges</li>
+    <li>Mangos</li>
+  </ul>,
+  document.getElementById('root')
+);
