eeee4b9e4139db80478150618f709ed00b6766a5
Raymond Gregory <vinedsel@gmail.com>
2018-12-11 14:49:59 -0600

    Modded JS file

--- src/index.js
+++ src/index.js
@@ -4,8 +4,17 @@ import ReactDOM from 'react-dom';
 ReactDOM.render(
   <ul>
     <li>Apples</li>
+    <ul>
+    <li>$1.00 per lb.</li>
+    </ul>
     <li>Oranges</li>
+    <ul>
+    <li>$.70 per lb.</li>
+    </ul>
     <li>Mangos</li>
+    <ul>
+    <li>$2.00 per lb.</li>  
+    </ul>
   </ul>,
   document.getElementById('root')
 );
